9131969255ea7641ca94e28fe602c190ff1f724c
Sabrina Mostafa <78508655+sabrina-mostafa@users.noreply.github.com>
2022-11-02 07:52:23 -0700

    Create 7.js

---
+++ JS_Basic-practice/7.js
@@ -0,0 +1,9 @@
+//  Display a live clock using js.
+
+let a;
+    let time;
+    setInterval(() => {
+      a = new Date();
+      time = a.getHours() + ':' + a.getMinutes() + ':' + a.getSeconds();
+      document.getElementById('time').innerHTML = time;
+    }, 1000);
