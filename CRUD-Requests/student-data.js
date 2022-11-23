1075d28520200df1a19b90777368ef4fd4ceb049
Sabrina Mostafa <78508655+sabrina-mostafa@users.noreply.github.com>
2022-11-23 03:35:11 -0800

    Create student-data.js

---
+++ CRUD-Requests/student-data.js
@@ -0,0 +1,65 @@
+//  for test-Data >>  https://mockaroo.com/
+
+const students = [{
+    "id": 1,
+    "first_name": "Livvyy",
+    "last_name": "Woolgar",
+    "email": "lwoolgar0@google.ru"
+  }, 
+  {
+    "id": 2,
+    "first_name": "Merrili",
+    "last_name": "McPhillips",
+    "email": "mmcphillips1@constantcontact.com"
+  }, 
+  {
+    "id": 3,
+    "first_name": "Innis",
+    "last_name": "Roma",
+    "email": "iroma2@ebay.co.uk"
+  }, 
+  {
+    "id": 4,
+    "first_name": "Vlad",
+    "last_name": "Weatherill",
+    "email": "vweatherill3@paypal.com"
+  }, 
+  {
+    "id": 5,
+    "first_name": "Cara",
+    "last_name": "Harriday",
+    "email": "charriday4@webeden.co.uk"
+  }, 
+  {
+    "id": 6,
+    "first_name": "Chloe",
+    "last_name": "Haliburn",
+    "email": "chaliburn5@exblog.jp"
+  }, 
+  {
+    "id": 7,
+    "first_name": "Dionisio",
+    "last_name": "Wyles",
+    "email": "dwyles6@springer.com"
+  }, 
+  {
+    "id": 8,
+    "first_name": "Allina",
+    "last_name": "Cornuau",
+    "email": "acornuau7@ucoz.ru"
+  },
+  {
+    "id": 9,
+    "first_name": "Stacy",
+    "last_name": "Minchinden",
+    "email": "sminchinden8@51.la"
+  }, 
+  {
+    "id": 10,
+    "first_name": "Karlee",
+    "last_name": "Dunbar",
+    "email": "kdunbar9@indiatimes.com"
+  }]
+
+  module.exports = students ;   // exporting students so that we can access it in other files
+  
